57a1eb962282c3949a60e2a2ed891e601e7cded8
odteam-x <oscar2008odns@gmail.com>
2026-03-15 21:18:09 -0400

    Add files via upload

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
  * TOKEN debe coincidir exactamente con API_TOKEN en Code.gs.
  */
 const PORTAL_CONFIG = Object.freeze({
-  API_URL:        'https://script.google.com/macros/s/AKfycbyIRFx5HOTc_MEwY7k_NhVb64DPQhihUxvY4Aqvg4LMr_YbT-CnSLUBh6oLFKh68bE/exec',
+  API_URL:        'https://script.google.com/macros/s/AKfycbxf9bsBoqzIQS-LJfyn27evNEL0uCoFjLK5K7po2vReepvjAa5g2QR_LwL170rc6j7l/exec',
   TOKEN:          'sti2026',
   AUTO_REFRESH_MS: 60_000,
   PERIODOS:       ['PE1', 'PE2', 'PE3'],
